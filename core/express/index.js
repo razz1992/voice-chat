01f68432482b415cf176562514a14e5781f91d0b
Rajaneesh <kumarrajneesh75@yahoo.co.in>
2020-04-15 15:04:52 +0530

    config changes for  deployment
    config changes for  deployment

--- core/express/index.js
+++ core/express/index.js
@@ -1,10 +1,11 @@
 const app=global.app=express();
+const PORT = process.env.PORT || 8080
 const options = {
   key: fs.readFileSync('testcert/key.pem'),
   cert: fs.readFileSync('testcert/cert.pem')
 };
 app.use('/', express.static(__dirname + '/../../client'));
-const server=global.server=https.createServer(options,app).listen(9980);
+const server=global.server=https.createServer(options,app).listen(PORT);
 server.on("listen",function() {
-  mainlog("Server","@",9980);
+  mainlog("Server","@",PORT);
 });
